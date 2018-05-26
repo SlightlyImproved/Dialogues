fe35b162da0dfa4854132683ec55f85651261291
Arthur Corenzan <arthur@corenzan.com>
2018-05-26 19:36:57 -0300

    Rework the code; use different hooks to if it fixes some issues on some edge cases

--- SlightlyImprovedDialogues.lua
+++ SlightlyImprovedDialogues.lua
@@ -4,106 +4,80 @@
 local NAMESPACE = "SlightlyImprovedDialogues"
 
 -- Uncomment to prevent debug messages
-local function d()  end
-local function df() end
+-- local function d()  end
+-- local function df() end
 
--- esoui\ingame\interactwindow\keyboard\interactwindow_keyboard.lua:2
-local SEEN_PLAYER_OPTION_COLOR = ZO_ColorDef:New(GetInterfaceColor(INTERFACE_COLOR_TYPE_GENERAL, INTERFACE_GENERAL_COLOR_DISABLED))
-local CHATTER_OPTION_INDENT = 30
-local BACKGROUND_OFFSETX = -40
-local BACKGROUND_HEIGHT = 120
+-- See esoui/ingame/interactwindow/keyboard/interactwindow_keyboard.lua:2
+local chatterOptionIndent = 30
 
--- esoui\ingame\interactwindow\keyboard\interactwindow_keyboard.lua:209
-function ZO_Interaction:AnchorBottomBG(optionControl)
-    self.control:GetNamedChild("BottomBG"):ClearAnchors()
-    self.control:GetNamedChild("BottomBG"):SetAnchor(TOPRIGHT, GuiRoot, RIGHT)
-    self.control:GetNamedChild("BottomBG"):SetAnchor(BOTTOMLEFT, optionControl, BOTTOMLEFT, BACKGROUND_OFFSETX - CHATTER_OPTION_INDENT, BACKGROUND_HEIGHT)
-end
+-- See esoui/ingame/interactwindow/keyboard/interactwindow_keyboard.lua:13
+local chosenBeforeColor = ZO_ColorDef:New(GetInterfaceColor(INTERFACE_COLOR_TYPE_GENERAL, INTERFACE_GENERAL_COLOR_DISABLED))
 
-local function ChangeBackgrounOffsetX()
-    -- esoui\ingame\interactwindow\keyboard\interactwindow_keyboard.xml:126
-    ZO_InteractWindowTopBG:ClearAnchors()
-    ZO_InteractWindowTopBG:SetAnchor(BOTTOMRIGHT, GuiRoot, RIGHT)
-    ZO_InteractWindowTopBG:SetAnchor(TOPLEFT, ZO_InteractWindowTargetAreaTitle, nil, BACKGROUND_OFFSETX, -BACKGROUND_HEIGHT)
-end
+-- Updated position for the background.
+local backgroundOffsetX = -40 -- was -10.
+local backgroundHeight = 120 -- stayed the same.
 
-local function ChangeOptionsHighlight()
-    ZO_InteractWindowPlayerAreaHighlight:GetNamedChild("Top"):SetHidden(true)
-    ZO_InteractWindowPlayerAreaHighlight:GetNamedChild("Bottom"):SetHidden(true)
+-- The dialogue options get updated everytime there's a new dialogue, we so hook after this method and reapply our changes.
+local function hookInteractionPopulateChatterOption() 
+    local populateChatterOption = INTERACTION.PopulateChatterOption
+    function INTERACTION:PopulateChatterOption(...)
 
-    local highlightBackground = CreateControl("ZO_InteractWindowPlayerAreaHighlightBackground", ZO_InteractWindowPlayerAreaHighlight, CT_TEXTURE)
-    highlightBackground:SetTexture("esoui\\art\\buttons\\gamepad\\inline_controllerbkg_darkgrey-center.dds")
-    highlightBackground:SetTextureCoords(0.5, 1, 0, 1)
-    highlightBackground:SetAlpha(0.75)
-    highlightBackground:SetBlendMode(TEX_BLEND_MODE_ALPHA)
-    highlightBackground:SetAnchor(TOPLEFT, ZO_InteractWindowPlayerAreaHighlight, nil, -CHATTER_OPTION_INDENT + BACKGROUND_OFFSETX, -2)
-    highlightBackground:SetAnchor(BOTTOMRIGHT, ZO_InteractWindowPlayerAreaHighlight, nil, 0, 3)
-end
+        -- Invoke the original method.
+        populateChatterOption(self, ...)
 
-local function ChangeTargetTitle()
-    local title = ZO_InteractWindowTargetAreaTitle:GetText()
-    ZO_InteractWindowTargetAreaTitle:SetText(string.sub(title, 2, -2))
-    ZO_InteractWindowTargetAreaTitle:SetHorizontalAlignment(TEXT_ALIGN_LEFT)
-    ZO_InteractWindowTargetAreaTitle:SetFont("ZoFontCallout")
-end
+        local index = ...
+        local option = INTERACTION.optionControls[index]
 
-local function OverrideOptionsSetText()
-    for index, label in ipairs(INTERACTION.optionControls) do
-        local setText = label.SetText
-        function label:SetText(text)
-            local previousOptionsWithText = 0
-            for i = 1, index do
-                if INTERACTION.optionControls[i].optionText ~= "" then
-                    previousOptionsWithText = previousOptionsWithText + 1
-                end
-            end
-            setText(self, previousOptionsWithText..". "..text)
+        -- Add number prefix.
+        option:SetText(index..". "..option:GetText())
+
+        -- Always flag Goodbye as "seen before".
+        if (option.optionType == CHATTER_GOODBYE) then
+            option.chosenBefore = true
+            option:SetColor(chosenBeforeColor:UnpackRGBA())
         end
     end
 end
 
-local function ClearSelectedChatterOption()
-    if INTERACTION.currentMouseLabel ~= nil then
+-- For some reason there are times when there's only one important option.
+-- In these cases we skip the second press, since there's no point in confirming our only option.
+local function countImportantChatterOptions()
+    local count = 0
+    for index, label in ipairs(INTERACTION.optionControls) do
+        if label.isImportant then
+            count = count + 1
+        end
+    end
+    return count
+end
+
+-- Since we're emulating a "mouse over" with the first key press, it
+-- never is going to trigger a "mouse exit", so we emulate that too.
+local function exitInteractionCurrentMouseLabel()
+    if (INTERACTION.currentMouseLabel ~= nil) then
         ZO_ChatterOption_MouseExit(INTERACTION.currentMouseLabel)
     end
 end
 
-local function FlagGoodbyeAsChosenBefore(goodbyeAlwaysSeen)
-    if goodbyeAlwaysSeen then
-        local option = INTERACTION.optionControls[INTERACTION.optionCount]
-        if (option.optionType == CHATTER_GOODBYE) then
-            option:SetColor(SEEN_PLAYER_OPTION_COLOR:UnpackRGBA())
-        end
-    end
-end
+-- To add the "press again to confirm" when selecting important options with the keyboard we intercept 
+-- this method and instead of making the selection right away we first highlight the option by emulating 
+-- a "mouse over" and only if it's already highlighted we let it proceed as usual.
+local function hookInteractionSelectChatterOptionByIndex()
+    local selectChatterOptionByIndex = INTERACTION.SelectChatterOptionByIndex
 
-local function GetImportantChatterOptions()
-    local options = {}
-    for index, label in ipairs(INTERACTION.optionControls) do
-        if label.isImportant then
-            table.insert(options, label)
-        end
-    end
-    return options
-end
-
-local function HookSelectChatterOptionByIndex()
-    local SelectChatterOptionByIndex = INTERACTION.SelectChatterOptionByIndex
-
-    -- Source at esoui/ingame/interactwindow/keyboard/interactwindow_keyboard.lua:233
+    -- See esoui/ingame/interactwindow/keyboard/interactwindow_keyboard.lua:224
     function INTERACTION:SelectChatterOptionByIndex(optionIndex)
-        local label = INTERACTION.optionControls[optionIndex]
-        if label.isImportant then
-            if (INTERACTION.currentMouseLabel == label or #GetImportantChatterOptions() == 1) then
-                SelectChatterOptionByIndex(self, optionIndex)
+        local optionControl = INTERACTION.optionControls[optionIndex]
+        if optionControl.isImportant then
+            if (INTERACTION.currentMouseLabel == optionControl or countImportantChatterOptions() == 1) then
+                selectChatterOptionByIndex(self, optionIndex)
+                exitInteractionCurrentMouseLabel()
             else
-                if INTERACTION.currentMouseLabel ~= nil then
-                    ZO_ChatterOption_MouseExit(INTERACTION.currentMouseLabel)
-                end
-                ZO_ChatterOption_MouseEnter(label)
+                exitInteractionCurrentMouseLabel()
+                ZO_ChatterOption_MouseEnter(optionControl)
             end
         else
-            SelectChatterOptionByIndex(self, optionIndex)
+            selectChatterOptionByIndex(self, optionIndex)
         end
     end
 end
@@ -121,33 +95,58 @@ local defaultSavedVars = {
     goodbyeAlwaysSeen = true,
 }
 
-local function OnAddOnLoaded(event, addOnName)
+local function onAddOnLoaded(event, addOnName)
     if (addOnName == NAMESPACE) then
         local savedVars = ZO_SavedVars:New(NAMESPACE.."_SavedVars", 1, nil, defaultSavedVars)
 
-        ChangeOptionsHighlight()
-        ChangeBackgrounOffsetX()
-        OverrideOptionsSetText()
-        HookSelectChatterOptionByIndex()
+        -- Reposition the top part of the background.
+        -- See esoui/ingame/interactwindow/keyboard/interactwindow_keyboard.xml:131
+        ZO_InteractWindowTopBG:ClearAnchors()
+        ZO_InteractWindowTopBG:SetAnchor(TOPLEFT, ZO_InteractWindowTargetAreaTitle, nil, backgroundOffsetX, -backgroundHeight)
+        ZO_InteractWindowTopBG:SetAnchor(BOTTOMRIGHT, GuiRoot, RIGHT)
 
-        local function SlightlyImproveDialogue(eventCode, ...)
-            ChangeTargetTitle()
-            ClearSelectedChatterOption()
-            FlagGoodbyeAsChosenBefore(savedVars.goodbyeAlwaysSeen)
+        -- The bottom part of the background is repositioned dynamically, so we're overriding this method.
+        -- See esoui/ingame/interactwindow/keyboard/interactwindow_keyboard.lua:204
+        function INTERACTION:AnchorBottomBG(optionControl)
+            self.control:GetNamedChild("BottomBG"):ClearAnchors()
+            self.control:GetNamedChild("BottomBG"):SetAnchor(TOPRIGHT, GuiRoot, RIGHT)
+            self.control:GetNamedChild("BottomBG"):SetAnchor(BOTTOMLEFT, optionControl, BOTTOMLEFT, backgroundOffsetX - chatterOptionIndent, backgroundHeight)
         end
-        EVENT_MANAGER:RegisterForEvent(NAMESPACE, EVENT_CHATTER_BEGIN, SlightlyImproveDialogue)
-        EVENT_MANAGER:RegisterForEvent(NAMESPACE, EVENT_QUEST_OFFERED, SlightlyImproveDialogue)
-        EVENT_MANAGER:RegisterForEvent(NAMESPACE, EVENT_QUEST_COMPLETE_DIALOG, SlightlyImproveDialogue)
-        EVENT_MANAGER:RegisterForEvent(NAMESPACE, EVENT_CONVERSATION_UPDATED, SlightlyImproveDialogue)
 
-        local function OnGameCameraDeactivated()
-            if savedVars.unlockCamera and not keepLockedCamera[GetInteractionType()] then
+        -- Hide the old highlight effect.
+        ZO_InteractWindowPlayerAreaHighlight:GetNamedChild("Top"):SetHidden(true)
+        ZO_InteractWindowPlayerAreaHighlight:GetNamedChild("Bottom"):SetHidden(true)
+
+        -- Create the new highlight effect.
+        local highlight = CreateControl("ZO_InteractWindowPlayerAreaHighlightBackground", ZO_InteractWindowPlayerAreaHighlight, CT_TEXTURE)
+        highlight:SetTexture("esoui\\art\\buttons\\gamepad\\inline_controllerbkg_darkgrey-center.dds")
+        highlight:SetTextureCoords(0.5, 1, 0, 1)
+        highlight:SetAlpha(0.75)
+        highlight:SetBlendMode(TEX_BLEND_MODE_ALPHA)
+        highlight:SetAnchor(TOPLEFT, ZO_InteractWindowPlayerAreaHighlight, nil, -chatterOptionIndent + backgroundOffsetX, -2)
+        highlight:SetAnchor(BOTTOMRIGHT, ZO_InteractWindowPlayerAreaHighlight, nil, 0, 3)
+
+        -- Replace the title's format with one without the dashes.
+        ZO_CreateStringId("SI_INTERACT_TITLE_FORMAT", "<<1>>")
+
+        -- Restyle the label.
+        ZO_InteractWindowTargetAreaTitle:SetHorizontalAlignment(TEXT_ALIGN_LEFT)
+        ZO_InteractWindowTargetAreaTitle:SetFont("ZoFontCallout")
+
+        -- Hook up.
+        hookInteractionSelectChatterOptionByIndex()
+        hookInteractionPopulateChatterOption()
+
+        -- Unlock camera on interaction.
+        local function onGameCameraDeactivated()
+            if (savedVars.unlockCamera and not keepLockedCamera[GetInteractionType()]) then
                 SetInteractionUsingInteractCamera(false)
             end
         end
-        EVENT_MANAGER:RegisterForEvent(NAMESPACE, EVENT_GAME_CAMERA_DEACTIVATED, OnGameCameraDeactivated)
+        EVENT_MANAGER:RegisterForEvent(NAMESPACE, EVENT_GAME_CAMERA_DEACTIVATED, onGameCameraDeactivated)
 
+        -- Fire a callback so code can hook after this add-on.
         CALLBACK_MANAGER:FireCallbacks(NAMESPACE.."_OnAddOnLoaded", savedVars)
     end
 end
-EVENT_MANAGER:RegisterForEvent(NAMESPACE, EVENT_ADD_ON_LOADED, OnAddOnLoaded)
+EVENT_MANAGER:RegisterForEvent(NAMESPACE, EVENT_ADD_ON_LOADED, onAddOnLoaded)
